e06c0af6b88a221ad7af68a1ffb0ae5fde4fb675
konradkelly <konradky@gmail.com>
2026-05-04 13:16:57 -0700

    Implement DfsBinaryGroupFinder with DFS group detection and sorting

--- src/DfsBinaryGroupFinder.java
+++ src/DfsBinaryGroupFinder.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class DfsBinaryGroupFinder implements BinaryGroupFinder {
    /**
@@ -30,7 +32,74 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
     */
     @Override
     public List<Group> findConnectedGroups(int[][] image) {
-        return null;
+        if (image == null) throw new NullPointerException();
+            for (int[] row : image) {
+            if (row == null) throw new NullPointerException();
     }
+
+        if (image.length == 0 || image[0].length == 0) throw new IllegalArgumentException();
+            int[][] visited = new int[image.length][image[0].length];
+            List<Group> groups = findConnectedGroupsHelper(image, visited);
+            Collections.sort(groups, Collections.reverseOrder());
+            return groups;
+        }
     
+    private static List<Group> findConnectedGroupsHelper(int[][] image, int[][] visited) {
+        List<Group> groups = new ArrayList<>();
+        for (int row = 0; row < image.length; row++) {
+            for (int col = 0; col < image[0].length; col++) {
+                if (image[row][col] == 1 && visited[row][col] == 0) {
+                    int[] salamander = salamanderSearch(image, new int[]{row, col}, visited);
+                    int size = salamander[0];
+                    int centroidX = salamander[1] / size;
+                    int centroidY = salamander[2] / size;
+                    groups.add(new Group(size, new Coordinate(centroidX, centroidY)));
+                }
+            }
+        }
+        return groups;
+    }
+
+    private static int[] salamanderSearch(int[][] image, int[] current, int[][] visited) {
+        int row = current[0];
+        int col = current[1];
+
+        if (visited[row][col] == 1 || image[row][col] == 0) return new int[]{0, 0, 0};
+
+        visited[row][col] = 1;
+
+        int[] data = {1, col, row};
+
+        for (int[] pixel : adjacentPixels(image, current)) {
+            int[] vals = salamanderSearch(image, pixel, visited);
+            data[0] += vals[0];
+            data[1] += vals[1];
+            data[2] += vals[2];
+        }
+        return data;
+    }
+
+    private static List<int[]> adjacentPixels(int[][] image, int[] current) {
+        int curR = current[0];
+        int curC = current[1];
+        
+        List<int[]> adjacentPixels = new ArrayList<>();
+
+        int[][] directions = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+
+        for (int[] dir : directions) {
+            int newR = curR + dir[0];
+            int newC = curC + dir[1];
+
+            if (newR >= 0 && newR < image.length && newC >= 0 && newC < image[0].length) {
+                adjacentPixels.add(new int[]{newR, newC});
+            }
+        }
+        return adjacentPixels;
+    }
 }
