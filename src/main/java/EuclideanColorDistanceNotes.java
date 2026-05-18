4723f98bac118ee9e66bdcdf94930a2ba44d66e3
konradkelly <konradky@gmail.com>
2026-05-18 14:00:07 -0700

    Revert "AI Used: Migrate project to Maven structure and test layout"
    This reverts commit e36ea41560816f04bcfb4f7c230b96fa245b0238.

--- src/main/java/EuclideanColorDistanceNotes.java
+++
@@ -1,6 +0,0 @@
-/*
-The Euclidean Color distance calculates the distance between two Hexadecimal colors.
-In Euclidean space, colors are computed based on how close they approximate to the colors: green, red, blue;
-The algorithm takes in two 24-bit integers to determine their distance.
-
-*/
