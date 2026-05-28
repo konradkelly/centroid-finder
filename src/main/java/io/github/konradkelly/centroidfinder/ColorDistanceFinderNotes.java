35ad874c868c3c2f7c71812d1b30138fddd88b7f
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-05-27 18:34:55 -0700

    Added ColorDistanceFinder interface, Coordinate record, and documentation notes for color distance abstraction

---
+++ src/main/java/io/github/konradkelly/centroidfinder/ColorDistanceFinderNotes.java
@@ -0,0 +1,8 @@
+package io.github.konradkelly.centroidfinder;
+
+/*
+The Color Distance Finder is an interface that outlines a method that calculates
+the color distance between two colors. It states that the implementation
+should use bit shifting and masking. This interface is implemented by the
+EuclideanColorDistance class.
+ */
