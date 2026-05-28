61dd84bf99f4cc145ff2089e87890b151485bbf2
konradkelly <konradky@gmail.com>
2026-05-28 14:22:47 -0700

    fix: remove video branch files from server branch

--- src/main/java/io/github/konradkelly/centroidfinder/Coordinate.java
+++
@@ -1,9 +0,0 @@
-package io.github.konradkelly.centroidfinder;
-
-/**
- * Represents a location in an image or array.
- *
- * The top-left cell (row:0, column:0) is coordinate (x:0, y:0).
- * Y increases downward and X increases to the right.
- */
-public record Coordinate(int x, int y) {}
