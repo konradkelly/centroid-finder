4cafdf4106496ae1f4b1b4ec1683de5ed303c0a2
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-05-27 17:42:01 -0700

    Added Spring Boot application entry point with async support

---
+++ src/main/java/io/github/konradkelly/centroidfinder/ServerApplication.java
@@ -0,0 +1,15 @@
+package io.github.konradkelly.centroidfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableAsync
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
